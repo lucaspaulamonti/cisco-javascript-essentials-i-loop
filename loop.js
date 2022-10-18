aaa884f0b31c72dc7977013177109b909b408da2
lucaspaulamonti <lucasdepaulamonti@gmail.com>
2022-10-18 00:08:04 -0300

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,15 @@
+// Adicionando nomes em um array utilizando while e for.
+let names = [];
+let isOver = false;
+while (!isOver) {
+    let name = prompt("Enter another name or press cancel.");
+    if (name != null) {
+        names.push(name);
+    } else {
+        isOver = true;
+    }
+}
+
+for (let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
